4dfa82d79223bc797019bcbe6a0cc3dbd800e86c
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-05-06 18:30:35 +0800

    first use handlebars

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
-  res.send('ok')
+  res.render('index')
 })
+
 app.listen(port, () => {
   console.log('ok')
 })
