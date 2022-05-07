934b237dd015b2e53278794c56938e3a7885c249
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-05-07 20:54:35 +0800

    complete project

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -10,7 +11,22 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurants => {
+    return restaurants.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurants = restaurantList.results.find(restaurants =>
+    restaurants.id.toString() === req.params.restaurant_id)
+
+  res.render('show', { restaurants: restaurants })
 })
 
 app.listen(port, () => {
