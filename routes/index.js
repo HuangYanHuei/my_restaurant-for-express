bf520d6268f67efdc7223ba2c3cc41bd1125fedd
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-06-17 20:57:08 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+// 掛載 middleware
+const { authenticator } = require('../middleware/auth')
 // 引入路由模組
-router.use('/', home)
-router.use('/my_restaurant', restaurants)
+// 加入驗證程序
+router.use('/my_restaurant', authenticator, restaurants)
 router.use('/users', users)
+//寬鬆路由放在最後
+router.use('/', authenticator, home)
 // 匯出路由器
 module.exports = router
